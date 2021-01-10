dcfbb6ae27a5fe8d2380c27570176a99a371af6b
teacher <itstep.codeclub@gmail.com>
2021-01-10 05:12:20 +0200

    Добавлены настроки localhost для БД и исправлен баг в link

--- config.py
+++ config.py
@@ -5,10 +5,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '8650ff27b20705ec19f9f40a2bc15d76'
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
+"""
 app.config['MYSQL_DATABASE_HOST'] = 'softmasterlab.mysql.pythonanywhere-services.com'
 app.config['MYSQL_DATABASE_USER'] = 'softmasterlab'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Qwerty_1029384756'
 app.config['MYSQL_DATABASE_DB'] = 'softmasterlab$hrdep_db'
+"""
+
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'hrdep_db'
 
 mysql = MySQL()
 mysql.init_app(app)
